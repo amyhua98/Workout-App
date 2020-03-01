7cabb03d9d43ec2ed3d6de2ba5eff6b6c08b392f
Amy Hua <aymay98@gmail.com>
2020-02-29 18:22:48 -0600

    Initial Commit

---
+++ ahua_TabRemote/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ahua_TabRemote
+//
+//  Created by Amy Hua on 2/29/20.
+//  Copyright © 2020 DePaul University. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ahua_TabRemote/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ahua_TabRemote
+//
+//  Created by Amy Hua on 2/29/20.
+//  Copyright © 2020 DePaul University. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
